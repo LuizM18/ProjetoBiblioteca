a97e8b7d22ccbac4f71e658e73de83f25b456458
LuizM18 <luizmario2007deus@gmail.com>
2025-06-07 20:17:22 -0300

    Otimização de desempenho na sessão administrador

--- biblioteca-escolar/src/components/Footer.jsx
+++ biblioteca-escolar/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className="footer-custom mt-auto"> {/* Adicionado mt-auto do Bootstrap */}
             <div className="container">
                 <p>&copy; {new Date().getFullYear()} Biblioteca Escolar. Todos os direitos reservados.</p>
-                <p>Desenvolvido com <i className="bi bi-heart-fill text-danger"></i> por Você.</p>
+                <p>Desenvolvido com <i className="bi bi-heart-fill text-danger"></i> por LuizM.</p>
                 <p>Versão 1.0.0</p>
             </div>
         </footer>
